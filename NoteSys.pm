d55fe3946caa7688b4d4a7b567f5154dc7908ab4
John T. Wodder II <jwodder@sdf.lonestar.org>
2009-11-24 21:27:32 -0500

    Finished all of the basic translation & coding, assuming that all of the SQL statements are correct, which they probably aren't

--- NoteSys.pm
+++ NoteSys.pm
@@ -1,20 +1,13 @@
-# Add in some automatic sorting of notes returned?
-
-# Note: With the exception of 'fetchNote' and 'updateNote', all functions in
-# NoteSys that deal with notes take and return only note IDs.
-
 package NoteSys;
-
 require Exporter;
 our @ISA = ('Exporter');
 our $VERSION = v0.1;
 our @EXPORT = qw< connect abandon disconnect countNotes countTags fetchNote
- getNotesByTag getAllNotes getChildNotes updateNote >;
-
+ getTaggedNoteIDs getAllNoteIDs getChildNoteIDs updateNote deleteNote
+ createNote getTagsAndQtys >;
 use DBI;
 
 my $dbfile = '/Library/WebServer/Documents/db/notes.db';
-
 my($db, $getTags, $noteById, $getTaggedNotes, $getChildren, $addTag, $delTag);
 
 sub connect() {
@@ -22,10 +15,10 @@ sub connect() {
   PrintError => 0, RaiseError => 1}) or die "DBI->connect: " . $DBI::errstr;
  $getTags = $db->prepare('SELECT tag FROM taggings WHERE note=?');
   # Should the tags be sorted somehow?
- $noteById = $db->prepare('SELECT no, title, contents, created, edited,' .
-  ' parent FROM notes WHERE no=?');
+ $noteById = $db->prepare('SELECT idno, title, contents, created, edited,' .
+  ' parent FROM notes WHERE idno=?');
  $getTaggedNotes = $db->prepare('SELECT note FROM taggings WHERE tag=?');
- $getChildren = $db->prepare('SELECT no FROM notes WHERE parent=?');
+ $getChildren = $db->prepare('SELECT idno FROM notes WHERE parent=?');
  $addTag = $db->prepare('INSERT INTO taggings (note, tag) VALUES (?, ?)');
  $delTag = $db->prepare('DELETE FROM taggings WHERE note=? AND tag=?');
 }
@@ -43,44 +36,63 @@ sub fetchNote($) {
  my $id = shift;
  my @dat = $db->selectrow_array($noteById, {}, $id);
  # What should I do when the note doesn't exist?
- new Note id => $dat[0], title => $dat[1], contents => $dat[2], created =>
+ new Note idno => $dat[0], title => $dat[1], contents => $dat[2], created =>
   $dat[3], edited => $dat[4], parent => $dat[5],
   tags => $db->selectcol_arrayref($getTags, {}, $id);
 }
 
-sub getNotesByTag($) { # Returns the notes tagged with the given tag
+sub getTaggedNoteIDs($) { # Returns the note IDs with the given tag
  @{$db->selectcol_arrayref($getTaggedNotes, {}, $_[0])}
 }
 
-sub getAllNotes() { @{$db->selectcol_arrayref('SELECT no FROM notes')} }
-sub getChildNotes($) { @{$db->selectcol_arrayref($getChildren, {}, $_[0])} }
+sub getAllNoteIDs() { @{$db->selectcol_arrayref('SELECT idno FROM notes')} }
+sub getChildNoteIDs($) { @{$db->selectcol_arrayref($getChildren, {}, $_[0])} }
 
 sub updateNote($$) {
 # - Make sure that 'edited' is updated at some point in this function.
 # - Rewrite this so that only changed fields are updated.
-# - Somewhere (not necessarily in this function) delete tags that are no longer
-#   used.
  my($old, $new) = @_;
  my %oldTags = map { $_ => 0 } $old->tags;
  for ($new->tags) {
   if (exists $oldTags{$_}) { $oldTags{$_}++ }
-  else { $addTag->execute($old->id, $_) }
+  else { $addTag->execute($old->idno, $_) }
  }
  while (($tag, $kept) = each %oldTags) {
-  $delTag->execute($old->id, $tag) if !$kept
+  $delTag->execute($old->idno, $tag) if !$kept
  }
- $db->do('UPDATE notes SET title=?, contents=? WHERE no=?', {}, $new->title,
-  $new->contents, $old->id);
+ $db->do('UPDATE notes SET title=?, contents=? WHERE idno=?', {}, $new->title,
+  $new->contents, $old->idno);
 }
 
-sub createNote($);
-sub getTagQty($);
-sub deleteNote($);  # Takes a note ID
-sub getTagsAndQtys();  # ORDER BY tag COLLATE NOCASE ASC -- 'NOCASE' might be SQLite3-specific
+sub deleteNote($) { # Takes a note ID
+ my $id = shift;
+ $db->do('DELETE FROM notes WHERE idno=?', {}, $id);
+ $db->do('DELETE FROM taggings WHERE note=?', {}, $id);
+}
+
+sub createNote($) {
+# Make sure that 'created' and 'edited' are set at some point in this function.
+ my $new = shift;
+ $db->do('INSERT INTO notes (title, contents) VALUES (?, ?)', {}, $new->title,
+  $new->contents);
+ my $newid = $db->last_insert_id(undef, undef, 'notes', undef);
+  # The above line is highly non-portable ... not that this code is likely to
+  # ever be ported.
+ $addTag->execute($newid, $_) for @{$new->tags};
+}
+
+sub getTagsAndQtys() {
+ @{$db->selectall_arrayref('SELECT tag, COUNT(note) FROM taggings ORDER BY tag'
+  . ' COLLATE NOCASE ASC')}
+ # I doubt the above SQL statement is right.  Also, 'NOCASE' might be
+ # SQLite3-specific.
+}
 
 
 package Note;
-use Class::Struct id => '$', title => '$', contents => '$', tags => '@',
+use Class::Struct idno => '$', title => '$', contents => '$', tags => '@',
  created => '$', edited => '$', parent => '$';
 
-sub children { map { fetchNote $_ } getChildNotes($_[0]->id) }
+sub children { map { fetchNote $_ } getChildNoteIDs($_[0]->idno) }
+
+1;
--- notes.cgi
+++ notes.cgi
@@ -28,8 +28,8 @@ sub printNote($) {
  my $note = shift;
  print start_div({-style => 'margin-bottom: 2ex'}),
   b(escapeHTML($note->title)), ' &#x2014; ', a({-href => url(-relative => 1) .
-  '?edit=' . $note->id}, 'Edit'), ' ', a({-href => url(-relative => 1) .
-  '?del=' . $note->id}, 'Delete');
+  '?edit=' . $note->idno}, 'Edit'), ' ', a({-href => url(-relative => 1) .
+  '?del=' . $note->idno}, 'Delete');
  print $note->contents eq '' ? br : pre(map { escapeHTML "$_\n" }
   map { wrapLine($_, 80) } split /\n/, $note->contents);
  print join ', ', map {
@@ -48,6 +48,8 @@ sub parseTagList($) {
 
 print start_table({-border => 0, -align => 'center'}), start_Tr,
  start_td({-width => 500});
+print p({-style => 'font-size: 10px'}, countNotes, 'notes |', countTags,
+ 'tags');
 print p(a({-href => url(-relative => 1)}, 'All items') . ' | '
  . a({-href => url(-relative => 1) . '?new'}, 'New item'));
 
@@ -55,20 +57,20 @@ if (defined url_param('edit')) {
  my $old = fetchNote url_param('edit');
  # Check for errors!
  if (param) {
-  my $new = new Note id => $old->id, title => param('title'),
+  my $new = new Note idno => $old->idno, title => param('title'),
    contents => param('contents'), tags => [ parseTagList param('tags') ];
   # Add in something for the 'parent'?
   updateNote($old, $new);
   print p('Note edited');
  } else {
-  print start_form(-action => url(-relative => 1) . '?edit=' . $old->id);
+  print start_form(-action => url(-relative => 1) . '?edit=' . $old->idno);
   print textfield('title', $old->title, 80, 255);
   print br, tt(textarea('contents', $old->contents, 6, 80)), br;
   print textfield('tags', join(', ', @{$old->tags}), 80, 255);
   print br, submit(-value => 'Submit'), end_form;
  }
 } elsif (defined url_param('tag')) {
- map { printNote(fetchNote $_) } getNotesByTag(url_param('tag'))
+ map { printNote(fetchNote $_) } getTaggedNoteIDs(url_param('tag'))
   # ORDER BY no DESC
 } elsif (defined url_param('new')) {
  if (param) {
@@ -85,7 +87,7 @@ if (defined url_param('edit')) {
 } elsif (defined url_param('del')) {
  deleteNote(url_param('del'));
  print p('Note deleted');
-} else { map { printNote(fetchNote $_) } getAllNotes } # ORDER BY no DESC
+} else { map { printNote(fetchNote $_) } getAllNoteIDs } # ORDER BY no DESC
 
 print p(a({-href => url(-relative => 1)}, 'All notes') . ' | '
  . a({-href => url(-relative => 1) . '?new'}, 'New note'));
